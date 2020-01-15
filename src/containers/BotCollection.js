d9ca4f217a7d43b09e39255088811e2da01c80eb
Joseph Burks <joe_burks_3@yahoo.com>
2020-01-15 10:55:57 -0600

    added some sort buttons

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -6,12 +6,19 @@ class BotCollection extends React.Component {
 
   render(){
   	return (
-  	  <div className="ui four column grid">
-    		<div className="row">
-    		  {/*...and here..*/}
-    		  Collection of all bots
-    		</div>
-  	  </div>
+		<div>
+			<div className="fluid ui buttons">
+				<button id='health' className="ui button" onClick={this.props.onSort}>Health</button>
+				<button id='damage' className="ui button" onClick={this.props.onSort}>Damage</button>
+				<button id='armor' className="ui button" onClick={this.props.onSort}>Armor</button>
+			</div>
+			<div style={{height: 20}}></div>
+			<div className="ui four column grid">
+				<div className="row">
+					{this.props.bots.map(bot => <BotCard key={bot.id} bot={bot} onClick={this.props.onClick} />)}
+				</div>
+			</div>
+		</div>
   	);
   }
 
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,61 @@
 import React from "react";
+import BotCollection from './BotCollection'
+import YourBotArmy from './YourBotArmy'
+import BotSpecs from '../components/BotSpecs'
 
 class BotsPage extends React.Component {
-  //start here with your code for step one
+  
+  state = {
+    bots: [],
+    yourBots: [],
+    specsBot: null
+  }
+
+  componentDidMount(){
+    fetch('https://bot-battler-api.herokuapp.com/api/v1/bots')
+    .then(res => res.json())
+    .then(bots => this.setState({bots: bots}))
+  }
+
+  handleClick = clickedBot => {
+    this.state.specsBot ?
+    this.setState({specsBot: null})
+    : this.setState({specsBot: clickedBot})
+  }
+
+  handleEnlist = clickedBot => {
+    if(this.state.bots.includes(clickedBot)){
+      this.setState({
+        bots: this.state.bots.filter(bot => bot !== clickedBot),
+        yourBots: [...this.state.yourBots, clickedBot],
+        specsBot: null
+      })
+    }
+    if(this.state.yourBots.includes(clickedBot)){
+      this.setState({
+        bots: [...this.state.bots, clickedBot],
+        yourBots: this.state.yourBots.filter(bot => bot !== clickedBot)
+      })
+    }
+  }
+
+  handleSort = e => {
+    if(e.target.id === 'health'){
+      this.setState({bots: this.state.bots.sort((a, b) => (a.health > b.health) ? -1 : 1)})
+    }
+    if(e.target.id === 'damage'){
+      this.setState({bots: this.state.bots.sort((a, b) => (a.damage > b.damage) ? -1 : 1)})
+    }
+    if(e.target.id === 'armor'){
+      this.setState({bots: this.state.bots.sort((a, b) => (a.armor > b.armor) ? -1 : 1)})
+    }
+  }
 
   render() {
     return (
       <div>
-        {/* put your components here */}
+        <YourBotArmy bots={this.state.yourBots} onClick={this.handleEnlist}/>
+        {this.state.specsBot ? <BotSpecs bot={this.state.specsBot} onClick={this.handleClick} onEnlist={this.handleEnlist}/> : <BotCollection bots={this.state.bots} onClick={this.handleClick} onSort={this.handleSort}/>}
       </div>
     );
   }
